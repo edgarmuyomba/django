0ecc5ea63eea4fa720d9ad55348753c6ab924c0e
edgarmatthew <edgarmatthew247@gmail.com>
2023-03-04 15:39:21 +0300

    homePage jotter

--- Jotter/core/models.py
+++ Jotter/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=25)
+    followers = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    uuid = models.UUIDField(default=uuid4)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.CharField(max_length=155)
+    dateAdded = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.content[:50]}...'
--- Jotter/core/views.py
+++ Jotter/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post, Topic
 
-# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    topics = Topic.objects.all()
+    context = {'posts': posts, 'topics': topics}
+    return render(request, 'core/index.html', context)
