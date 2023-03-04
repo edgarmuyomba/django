e8c48433dc9ce4b03580afcf5edaf1803827d3e9
edgarmatthew <edgarmatthew247@gmail.com>
2023-03-04 16:17:27 +0300

    static files configuration

--- Jotter/core/models.py
+++ Jotter/core/models.py
@@ -3,17 +3,17 @@ from uuid import uuid4
 
 class Topic(models.Model):
     title = models.CharField(max_length=25)
-    followers = models.IntegerField()
+    followers = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class Post(models.Model):
-    uuid = models.UUIDField(default=uuid4)
+    uuid = models.UUIDField(default=uuid4, auto_created=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.CharField(max_length=155)
     dateAdded = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField()
+    likes = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.content[:50]}...'
+        return f'{self.text[:50]}...'
---
+++ Jotter/core/one.py
@@ -0,0 +1,13 @@
+import models
+
+topics = ['Technology','Sports','Politics','Entertainment','Food','Travel','Science','Health','Fashion','Business']
+
+posts = {'Technology': "10 new features in the latest iPhone release",'Sports': "How the underdog team won the championship",'Politics': "Why the new immigration policy is causing controversy",'Entertainment': "Top 10 movies of the year so far",'Food': "5 delicious recipes to try this weekend",'Travel': "Hidden gems to visit on your next vacation",'Science': "The latest breakthrough in medical research",'Health': "10 tips for staying healthy during flu season",'Fashion': "The hottest trends for the upcoming season",'Business': "How to start a successful online business"}
+
+for topic in topics:
+    newTopic = models.Topic(title = topic)
+    newTopic.save()
+
+for k,v in posts.items:
+    newPost = models.Post(topic=k, text=v)
+    newPost.save()
